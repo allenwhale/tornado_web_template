1fb7f2d28fffcc59797b335ccd129e3eb9d2f44d
allenwhale <allenwhale@yahoo.com.tw>
2016-09-11 14:20:56 +0800

    update form add err msg format

--- backend/utils/form.py
+++ backend/utils/form.py
@@ -1,12 +1,23 @@
 from dateutil import parser
 from datetime import datetime
-from utils.pxfilter import XssHtml
+
+
+global ERR_FORMAT
+ERR_FORMAT = {}
+ERR_FORMAT['REQUIRE'] = '{desc} not in form'
+ERR_FORMAT['TYPE'] = '{desc}: {exception}'
+ERR_FORMAT['NON_EMPTY'] = '{desc} cannot be empty'
+ERR_FORMAT['EXCEPT'] = '{desc} cannot be one of {except}'
+ERR_FORMAT['RANGE'] = '{desc} must in range {range}'
+ERR_FORMAT['LEN_RANGE'] = 'length of {desc} must in range {len_range}'
 
 
 def form_validation(form, schema):
     err = _form_validation(form, schema)
     return (400, err) if err else None
 
+def register_err_format(err_format):
+    ERR_FORMAT.update(err_format)
 
 def _form_validation(form, schema):
     '''
@@ -15,13 +26,13 @@ def _form_validation(form, schema):
             # require
             'name': <str> # +<str> means require, default is optional
             # optional
+            'desc': <str>
             'type': <class>
             'non_empty': <bool> # for str, list
             'except': <list>
             'range': <tuple> # t[0] <= value <= t[1]
             'len_range': <tuple> # t[0] <= len(value) <= t[1]
             'check_dict': <dict> # for dict
-            'xss': <bool> # xss filter, default is False
             ...
         }]
     int
@@ -44,64 +55,58 @@ def _form_validation(form, schema):
     for item in schema:
         require = True if item['name'][0] == '+' else False
         name = item['name'] = item['name'][1:] if require else item['name']
+        desc = item['desc'] = item.get('desc') or name
 
         # check require
         if require and (name not in form or form[name] is None):
-            return '%s not in form' % name
+            return ERR_FORMAT['REQUIRE'].format(**item)
 
         
         if not require and (name not in form or form[name] is None):
             form[name] = None
             continue
 
-        ## check non_empty
-        if 'non_empty' in item and item['non_empty']:
-            if form[name] == item['type']() or form[name] is None:
-                return 'value of %s: "%s" should not be empty value' % (name, str(form[name]))
+        item['value'] = form[name]
 
         ### check value type
         if 'type' in item:
             if not isinstance(form[name], item['type']):
                 if item['type'] == datetime:
-                    try: form[name] = parser.parse(form[name])
-                    except Exception as e: return name + str(e)
+                    try: 
+                        form[name] = item['value'] = parser.parse(form[name])
+                    except Exception as e: 
+                        item['exception'] = e
+                        return ERR_FORMAT['TYPE'].format(**item)
                 else:
-                    try: form[name] = item['type'](form[name])
-                    except Exception as e: return name + str(e)
+                    try: 
+                        form[name] = item['value'] = item['type'](form[name])
+                    except Exception as e: 
+                        item['exception'] = e
+                        return ERR_FORMAT['TYPE'].format(**item)
 
+        ## check non_empty
+        if 'non_empty' in item and item['non_empty']:
+            if form[name] == item['type']() or form[name] is None:
+                return ERR_FORMAT['NON_EMPTY'].format(**item)
 
         ### check except
         if 'except' in item:
             if form[name] in item['except']:
-                return 'value of %s: "%s" in except list' % (name, str(form[name]))
+                return ERR_FORMAT['EXCEPT'].format(**item)
         
         ### check range
         if 'range' in item:
             if not (item['range'][0] <= form[name] <= item['range'][1]):
-                return 'value of %s: "%s" not in range %s' % (name, str(form[name]), str(item['range']))
+                return ERR_FORMAT['RANGE'].format(**item)
 
         ### check len_range
         if 'len_range' in item:
             if not (item['len_range'][0] <= len(form[name]) <= item['len_range'][1]):
-                return 'value of %s: "%s" not in len_range %s' % (name, str(form[name]), str(item['len_range']))
+                return ERR_FORMAT['LEN_RANGE'].format(**item)
 
         ### check check_dict
         if 'check_dict' in item:
             err = form_validation(form[name], item['check_dict'])
             if err: return err
-
-        ### check xss
-        xss_check = False
-        if 'xss' in item and item['xss'] == False:
-            pass
-        elif 'xss' in item and item['xss'] == True:
-            xss_check = True
-        elif 'type' in item and item['type'] == str:
-            xss_check = True
-        if xss_check:
-            xss = XssHtml()
-            xss.feed(form[name])
-            xss.close()
-            form[name] = xss.getHtml()
 
     return None
